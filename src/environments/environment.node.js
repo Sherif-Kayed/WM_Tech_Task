ca2f3403cb02400e60d87fef34dc0d3c0fca7a8a
SneakyMeligy <sherifkayed94@gmail.com>
2023-10-10 17:26:51 +0200

    Added the environment files and removed them from gitignore

---
+++ src/environments/environment.node.js
@@ -0,0 +1,6 @@
+const env = {
+    production: false,
+    apiUrl: 'https://37f32cl571.execute-api.eu-central-1.amazonaws.com/default/wunderfleet-recruiting-backend-dev-save-payment-data'
+}
+
+module.exports = env;
---
+++ src/environments/environment.secret.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://37f32cl571.execute-api.eu-central-1.amazonaws.com/default/wunderfleet-recruiting-backend-dev-save-payment-data'
+}
